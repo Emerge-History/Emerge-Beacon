df60c2dd1ff0bd8b3cb803cfbcb0da686dfc327c
ouzhou <627290897@qq.com>
2016-09-08 00:46:55 +0800

    添加md5后缀

--- web/gulpfile.babel.js
+++ web/gulpfile.babel.js
@@ -2,131 +2,155 @@
 
 import gulp from 'gulp';
 import less from 'gulp-less';
-import path from 'path';
+// import path from 'path';
 import autoprefixer from 'gulp-autoprefixer';
-import minifycss from 'gulp-minify-css';
-import browserSync from 'browser-sync';
-import uglify from 'gulp-uglify';
-import concat from 'gulp-concat';
-import rename from 'gulp-rename';
-import notify from 'gulp-notify';
-import clean from 'gulp-clean';
-import htmlmin from 'gulp-htmlmin';
+import cleanCSS from 'gulp-clean-css';
 import plumber from 'gulp-plumber';
-import spriter from 'gulp-sprite2';
-import imagemin from 'gulp-imagemin';
-import rev from 'gulp-rev';
-import revCollector from 'gulp-rev-collector';
+import clean from 'gulp-clean';
+import browserSync from 'browser-sync';
 import runSequence from 'gulp-run-sequence';
+import imagemin from 'gulp-imagemin';
+import uglify from 'gulp-uglify';
+import rev from 'gulp-rev';
+import htmlmin from 'gulp-htmlmin';
+import revCollector from 'gulp-rev-collector';
+
 
 import {paths} from './gulpfile.paths';
-import {_task} from './gulpfile.task';
-
 const reload = browserSync.reload;
-
-
-
-
 
 // css
 gulp.task('css', () => {
-  return gulp.src(paths.src.css)
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(paths.dev._css))
-    .pipe(reload({ stream: true }));
+    return gulp.src(paths.src.css)
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(paths.dev._css))
+        .pipe(reload({ stream: true }));
 });
+
 gulp.task('css-min', ['css'], () => {
-  return gulp.src(paths.dev.css)
-    //.pipe(concat('wap.min.css'))
-    .pipe(minifycss())
-    .pipe(rev())
-    .pipe(gulp.dest(paths.dist._css))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('./dev/rev/css'));
+    return gulp.src(paths.dev.css)
+        //.pipe(concat('all.min.css'))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rev())
+        .pipe(gulp.dest(paths.dist._css))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(paths.dev._rev + '/css'));
+});
+
+
+// fonts
+gulp.task('fonts', () => {
+    return gulp.src(paths.src.fonts)
+        .pipe(gulp.dest(paths.dev._fonts))
+});
+
+gulp.task('fonts-min', ['fonts'], () => {
+    return gulp.src(paths.dev.fonts)
+        .pipe(gulp.dest(paths.dist._fonts))
 });
 
 // html
-gulp.task('html', function () {
-  return gulp.src(paths.src.html)
-    .pipe(gulp.dest(paths.dev._html))
-    .pipe(reload({ stream: true }));
+gulp.task('html', () => {
+    return gulp.src(paths.src.html)
+        .pipe(gulp.dest(paths.dev._html))
+        .pipe(reload({ stream: true }));
 });
-gulp.task('html-rev', ['html'], function () {
-  gulp.src(['./dev/rev/**/*.json', paths.dev.html])
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(revCollector({replaceReved: true}))
-    .pipe(gulp.dest(paths.dist._html));
+
+gulp.task('html-min', ['html'], () => {
+    gulp.src([paths.dev.rev, paths.dev.html])
+        .pipe(revCollector({ replaceReved: true }))
+        // .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(paths.dist._html));
 });
 
 // img
-gulp.task('img', function () {
-  return gulp.src(paths.src.img)
-    .pipe(gulp.dest(paths.dev._img))
-    .pipe(reload({ stream: true }));
+gulp.task('img', () => {
+    return gulp.src(paths.src.img)
+        .pipe(gulp.dest(paths.dev._img))
+        .pipe(reload({ stream: true }));
 });
-gulp.task('img-min', ['img'], function () {
-  return gulp.src(paths.dev.img)
-    .pipe(imagemin())
-    .pipe(gulp.dest(paths.dist._img))
+
+gulp.task('img-min', ['img'], () => {
+    return gulp.src(paths.dev.img)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.dist._img))
 });
 
 // js
-gulp.task('js', function () {
-  return gulp.src(paths.src.js)
-    .pipe(gulp.dest(paths.dev._js))
-    .pipe(reload({ stream: true }));
+gulp.task('js', () => {
+    return gulp.src(paths.src.js)
+        .pipe(gulp.dest(paths.dev._js))
+        .pipe(reload({ stream: true }));
 });
-gulp.task('js-min', ['js'], function () {
-  return gulp.src(paths.dev.js)
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.dist._js))
+
+gulp.task('js-min', ['js'], () => {
+    return gulp.src(paths.dev.js)
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest(paths.dist._js))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(paths.dev._rev + '/js'));
 });
+
+
+
 
 // favicon
-gulp.task('favicon', function () {
-  return gulp.src(paths.src.favicon)
-    .pipe(gulp.dest(paths.dev._path))
+gulp.task('favicon', () => {
+    return gulp.src(paths.src.favicon)
+        .pipe(gulp.dest(paths.dev._path))
 });
-gulp.task('favicon-min', ['favicon'], function () {
-  return gulp.src(paths.dev.favicon)
-    .pipe(gulp.dest(paths.dist._path))
+
+gulp.task('favicon-min', ['favicon'], () => {
+    return gulp.src(paths.src.favicon)
+        .pipe(gulp.dest(paths.dev._path))
 });
+
+
 
 // clean
-gulp.task('clean', function () {
-  return gulp.src(paths.dev._path, {
-    read: false
-  })
-    .pipe(clean());
+gulp.task('clean', () => {
+    return gulp.src(paths.dev._path, {
+        read: false
+    })
+        .pipe(clean());
+});
+gulp.task('clean-min', () => {
+    return gulp.src([paths.dist._path, paths.dist._html], {
+        read: false
+    })
+        .pipe(clean({ force: true }));
 });
 
 
-gulp.task('default', ['dev'], function () {
-  _task({
-    projectName: 'Example'
-  })
+gulp.task('default', ['dev'], () => {
+
 });
 
-gulp.task('dev', ['css', 'html', 'img', 'js', 'favicon'], function () {
-  browserSync({
-    files: [
-      paths.dev.css,
-      paths.dev.html,
-      paths.dev.img,
-      paths.dev.js
-    ],
-    server: {
-      baseDir: paths.dev._path
-    }
-  });
-  gulp.watch(paths.src.css, ['css']);
-  gulp.watch(paths.src.html, ['html']);
-  gulp.watch(paths.src.img, ['img']);
-  gulp.watch(paths.src.js, ['js']);
+gulp.task('dev', (cb) => {
+    runSequence('clean', ['css', 'fonts', 'html', 'img', 'js', 'favicon'], cb);
+    browserSync({
+        files: [
+            paths.dev.css,
+            paths.dev.html,
+            paths.dev.img,
+            paths.dev.js
+        ],
+        server: {
+            baseDir: paths.dev._path
+        }
+    });
+    gulp.watch(paths.src.css, ['css']);
+    gulp.watch(paths.src.html, ['html']);
+    gulp.watch(paths.src.img, ['img']);
+    gulp.watch(paths.src.js, ['js']);
 });
 
-gulp.task('dist', function (cb) {
-  runSequence('css-min', 'img-min', 'js-min', 'favicon-min', 'html-rev', cb);
+gulp.task('dist', (cb) => {
+    runSequence('clean', 'clean-min', 'css-min', 'js-min', ['fonts-min', 'html-min', 'img-min', 'favicon-min'], 'clean', cb);
 });
--- web/gulpfile.paths.js
+++ web/gulpfile.paths.js
@@ -4,6 +4,7 @@ const paths = {
         img: './src/img/*.{jpg,png,gif,svg}',
         js: './src/js/*.js',
         css: './src/css/style-*.less',
+        fonts: './src/fonts/*.{eot,svg,ttf,woff,woff2}',
         favicon: './src/favicon.ico'
     },
     dev: {
@@ -12,18 +13,23 @@ const paths = {
         js: './dev/js/*.js',
         css: './dev/css/style-*.css',
         favicon: './dev/favicon.ico',
+        fonts: './dev/fonts/*.{eot,svg,ttf,woff,woff2}',
+        rev: './dev/rev/**/*.json',
         _path: './dev',
         _html: './dev/html',
         _img: './dev/img',
         _js: './dev/js',
-        _css: './dev/css'
+        _css: './dev/css',
+        _fonts: './dev/fonts',
+        _rev: './dev/rev'
     },
     dist: {
         _path: '../server/public',
         _html: '../server/views',
         _img: '../server/public/img',
         _js: '../server/public/js',
-        _css: '../server/public/css'
+        _css: '../server/public/css',
+        _fonts: '../server/public/fonts'
     }
 };
 
